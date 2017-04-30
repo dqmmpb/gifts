e759f20fcb403708c6521445dfd8faad922e1e33
邓启明 <dengqiming@xiaomadada.com>
2017-04-30 15:41:53 +0800

    修改qrcode样式

--- src/components/Gift/qrCode.vue
+++ src/components/Gift/qrCode.vue
@@ -1,29 +1,22 @@
 <template>
   <div>
-    <div class="panel-padding text-align-center">
-      <div>领取成功</div>
+    <div class="panel-padding text-align-center panel-qrcode">
+      <div class="qrcode-msg">领取成功</div>
       <div class="wechat">
         <img src="../../assets/qrcode.png" class="qrcode">
-        <div>长按识别二维码关注哦</div>
       </div>
-      <div>关注公众号，进入"进入物流信息"中可查看物流动态</div>
+      <div class="qrcode-msg-2">
+        <div>长按识别二维码关注哦</div>
+        <div>关注公众号，进入"物流信息"中可查看物流动态</div>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 
-import { Tabbar, Group, Cell, XInput, XAddress, XTextarea, XButton } from 'vux'
-
 export default {
   components: {
-    Tabbar,
-    Group,
-    Cell,
-    XInput,
-    XAddress,
-    XTextarea,
-    XButton
   },
   methods: {
     initPage () {
